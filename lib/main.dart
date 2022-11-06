0e42cce9343bf2a334cef5b6e0dd526eadf8ea9c
kawano <kawano@adminnoMacBook-Pro.local>
2022-11-06 23:43:22 +0900

    create base

--- lib/main.dart
+++ lib/main.dart
@@ -14,134 +14,158 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   title: 'Flutter Demo',
-    //   theme: ThemeData(
-    //     primarySwatch: Colors.blue,
-    //   ),
-    //   home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    // );
-    return  CupertinoApp(
-      // navigatorKey: ,
-      home: MyHomePage(title:'my_app'),
-      routes: 
-        {
-          '/second': (context) => SecondPage(),
-        },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-  // Future<void> toScecondPage() async {
-  //   Navigator.of(context).pushNamed('/second');
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-
-
-    return  CupertinoPageScaffold(
-      navigationBar:  const CupertinoNavigationBar(
-        middle: Text('home'),
-        ),
-      child: SafeArea(
-        child: Center(
-          child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-               Conte1(),
-               ElevatedButton(
-                onPressed: (() => AppNav.toSecondPage(context)), 
-                child: const Text("SecondPage"),
-              )
-            ],
-          ),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
-    );
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     child: Column(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         const Text(
-    //           'You have pushed the button this many times:',
-    //         ),
-    //         Text(
-    //           '$_counter',
-    //           style: Theme.of(context).textTheme.headline4,
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
-  }
-}
-
-class Conte1 extends StatelessWidget {
-  const Conte1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      color: Colors.grey,
-      child: Text('aaaaa'),
+      home: const MainPage(),
     );
   }
 }
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+
+  }
+
+class _MainPageState extends State<MainPage> {
+  static const _screens = [
+      HomeScreen(),
+      SearchScreen(),
+      NotificationScreen(),
+      ReactionScreen(),
+      AccountScreen()
+    ];
+
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _showNewPostOrverlay(){
+    
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar:  const CupertinoNavigationBar(
-        middle: Text('second'),
-        ),
-        child: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Conte1(),
-                Text("secondpage")
-              ],
-            )
-          )
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: '探す'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: 'お知らせ'),
+          BottomNavigationBarItem(icon: Icon(Icons.emoji_people), label: '反響'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'マイページ'),
+        ],
+        type: BottomNavigationBarType.fixed,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showNewPostOrverlay,
+        tooltip: "Create New Post",
+        child: Icon(Icons.add),
         ),
     );
   }
 }
+
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('アカウント'),
+      ),
+      body: const Center(
+          child: Text('アカウント画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('探す'),
+      ),
+      body: const Center(
+          child: Text('探す画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
+
+class ReactionScreen extends StatelessWidget {
+  const ReactionScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('反響'),
+      ),
+      body: const Center(
+          child: Text('反響画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ホーム'),
+      ),
+      body:
+          const Center(child: Text('ホーム画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('お知らせ'),
+      ),
+      body:
+          const Center(child: Text('お知らせ画面', style: TextStyle(fontSize: 32.0))),
+    );
+  }
+}
+
+
 
 
 class AppNav {
 
-  static Future<void> toSecondPage(BuildContext context){
-    return Navigator.of(context).pushNamed('/second');
+  static Future<void> toHomeSecondPage(BuildContext context){
+    return Navigator.of(context).pushNamed('/home/second');
   }
+
+  static Future<void> toSearchSecondPage(BuildContext context){
+    return Navigator.of(context).pushNamed('/search/second');
+  }
+
+  
 }

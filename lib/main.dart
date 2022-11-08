1d48a2367f3ce37717afb4d67d7e80ba072089d0
kawano <kawano@adminnoMacBook-Pro.local>
2022-11-09 00:22:40 +0900

    bottomNav実装（遷移するたび初期化される）

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,59 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp( MyApp());
 }
 
+class HomeRoutes{
+    static final homeRoutes = <String,WidgetBuilder>{
+    "/" : (context) => HomeScreen(),
+    "/home/second" : (context) => NewPost(),
+  };
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static NavigatorState get navigatorState => navigatorKey.currentState!;
+
+}
+class SearchRoutes{
+    static final searchRoutes = <String,WidgetBuilder>{
+    "/" : (context) => SearchScreen(),
+    "/search/second" : (context) => NewPost(),
+  };
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static NavigatorState get navigatorState => navigatorKey.currentState!;
+
+}
+class NotificationRoutes{
+    static final notificationRoutes = <String,WidgetBuilder>{
+    "/" : (context) => NotificationScreen(),
+    "/notification/second" : (context) => NewPost(),
+  };
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static NavigatorState get navigatorState => navigatorKey.currentState!;
+}
+class ReactionRoutes{
+    static final reactionRoutes = <String,WidgetBuilder>{
+    "/" : (context) => ReactionScreen(),
+    "/reaction/second" : (context) => NewPost(),
+  };
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static NavigatorState get navigatorState => navigatorKey.currentState!;
+}
+class AccountRoutes{
+    static final accountRoutes = <String,WidgetBuilder>{
+    "/" : (context) => AccountScreen(),
+    "/account/second" : (context) => NewPost(),
+  };
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  static NavigatorState get navigatorState => navigatorKey.currentState!;
+}
+
+
+
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  final GlobalKey<NavigatorState> _navkey = GlobalKey();
+  final GlobalKey<NavigatorState> _rootNavigationKey = GlobalKey();
 
   // This widget is the root of your application.
   @override
@@ -20,6 +64,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: const MainPage(),
+      routes: {
+        "/addNewPost" : (_) => NewPost(),
+      },
     );
   }
 }
@@ -33,12 +80,52 @@ class MainPage extends StatefulWidget {
   }
 
 class _MainPageState extends State<MainPage> {
-  static const _screens = [
-      HomeScreen(),
-      SearchScreen(),
-      NotificationScreen(),
-      ReactionScreen(),
-      AccountScreen()
+  final  _screens = [
+
+      Navigator(
+        key: HomeRoutes.navigatorKey,
+        onGenerateRoute: (settings) {
+          var builder = HomeRoutes.homeRoutes[settings.name]!;
+          return MaterialPageRoute(builder: builder,settings: settings);
+          // if(settings.name == "/"){
+          //   return MaterialPageRoute(builder: (context) => HomeScreen(),settings: settings);
+          // }
+        },
+      ) ,
+      Navigator(
+        key: SearchRoutes.navigatorKey,
+        onGenerateRoute: (settings) {
+          var builder = SearchRoutes.searchRoutes[settings.name]!;
+          return MaterialPageRoute(builder: builder,settings: settings);
+        },
+      ) ,
+      
+      Navigator(
+        key: NotificationRoutes.navigatorKey,
+        onGenerateRoute: (settings) {
+          var builder = NotificationRoutes.notificationRoutes[settings.name]!;
+          return MaterialPageRoute(builder: builder,settings: settings);
+        },
+      ) ,
+      Navigator(
+        key: ReactionRoutes.navigatorKey,
+        onGenerateRoute: (settings) {
+          var builder = ReactionRoutes.reactionRoutes[settings.name]!;
+          return MaterialPageRoute(builder: builder,settings: settings);
+        },
+      ) ,
+      Navigator(
+        key: AccountRoutes.navigatorKey,
+        onGenerateRoute: (settings) {
+          var builder = AccountRoutes.accountRoutes[settings.name]!;
+          return MaterialPageRoute(builder: builder,settings: settings);
+        },
+      ) ,
+      // HomeScreen(),
+      // SearchScreen(),
+      // NotificationScreen(),
+      // ReactionScreen(),
+      // AccountScreen()
     ];
 
   int _selectedIndex = 0;
@@ -48,8 +135,8 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
-  void _showNewPostOrverlay(){
-    
+  Future<void> _showNewPostOrverlay()async{
+    AppNav.toNewPostOrverlay(context);
   }
 
   @override
@@ -135,13 +222,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future<void> toHomeSecond()async{
+      AppNav.toHomeSecondPage(context);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('ホーム'),
       ),
       body: SafeArea(
         child: Center(
-           child: Text('ホーム画面', style: TextStyle(fontSize: 32.0)),
+           child: Column(
+            children: [
+              Text('ホーム画面', style: TextStyle(fontSize: 32.0)),
+              ElevatedButton(
+                onPressed: toHomeSecond, 
+                child: Text("次へ"))
+            ],
+           )
         ),
       ),
     );
@@ -166,7 +264,23 @@ class NotificationScreen extends StatelessWidget {
   }
 }
 
+class NewPost extends StatelessWidget {
+  const NewPost({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('newpost'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Text('newpost', style: TextStyle(fontSize: 32.0)),
+        ),
+      ),
+    );
+  }
+}
 
 
 class AppNav {
@@ -178,6 +292,11 @@ class AppNav {
   static Future<void> toSearchSecondPage(BuildContext context){
     return Navigator.of(context).pushNamed('/search/second');
   }
+
+  static Future<void> toNewPostOrverlay(BuildContext context){
+    return Navigator.of(context).pushNamed('/addNewPost');
+  }
+
 
   
 }
